05ef263f800ab74a85685d3ec3b64591b2d144a2
jayati-jaiswal <125111884+undefined@users.noreply.github.com>
2023-07-14 21:26:14 +0000

    First commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# lwc-kpjhqs4z-fork-lk334d8a
-Created with WebComponents.dev
---
+++ src/grandchild.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+
+export default class Grandchild extends LightningElement {
+
+}
